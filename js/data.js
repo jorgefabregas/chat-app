829a38bb3fe686eb39eb9ec5ee05e143ab6fae57
jorgefabregas <jorge.fabregas22@gmail.com>
2020-01-06 11:34:02 -0500

    ssecond commit

---
+++ js/data.js
@@ -0,0 +1,24 @@
+const user = {
+    id: 456,
+    name: 'Charles Babbage',
+    img: 'img/456.jpg'
+  }
+  
+  const message = [
+    {
+       id: 1,
+       from: {
+          id: 123,
+          name: 'Ada Lovelace',
+          img: 'img/123.jpg'
+       },
+       time: {
+          date: 1,
+          month: 8,
+          year: 1843,
+          hour: 14,
+          minute: 59
+       },
+       text: `You should check out this little script I just wrote. 😂 lol`
+    }
+  ]
